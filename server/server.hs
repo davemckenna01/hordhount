45d3d585fbb7eb92e03d578f91208685d4ffe68e
Dave McKenna <davemckenna01@gmail.com>
2016-08-17 13:59:57 -0400

    play around with encodings

--- server/server.hs
+++ server/server.hs
@@ -1,24 +1,31 @@
 import Control.Monad (unless)
 import Network.Socket hiding (recv)
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.UTF8 as UTF8
 import Network.Socket.ByteString (recv, sendAll)
 
+dispatchRequest :: Socket -> S.ByteString -> IO ()
+dispatchRequest conn msg = do
+    let converted = UTF8.fromString "wobbalobbaΩdingdong"
+    sendAll conn converted
+
 main :: IO ()
-main = withSocketsDo $
-    do addrinfos <- getAddrInfo
-                    (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
-                    Nothing (Just "3000")
-       let serveraddr = head addrinfos
-       sock <- socket (addrFamily serveraddr) Stream defaultProtocol
-       bind sock (addrAddress serveraddr)
-       listen sock 1
-       (conn, _) <- accept sock
-       talk conn
-       close conn
-       close sock
+main = withSocketsDo $ do
+        addrinfos <- getAddrInfo
+                     (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
+                     Nothing (Just "3999")
+        let serveraddr = head addrinfos
+        sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+        bind sock (addrAddress serveraddr)
+        listen sock 1
+        (conn, _) <- accept sock
+        talk conn
+        close conn
+        close sock
 
     where
-      talk :: Socket -> IO ()
-      talk conn =
-          do msg <- recv conn 1024
-             unless (S.null msg) $ sendAll conn msg >> talk conn
+        talk :: Socket -> IO ()
+        talk conn = do
+            msg <- recv conn 1024
+            unless (S.null msg) $ dispatchRequest conn msg >> talk conn
