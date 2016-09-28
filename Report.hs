1b424d6a887e3b7b9c214994fced251c148739d1
Dave McKenna <davemckenna01@gmail.com>
2016-09-28 17:03:13 -0400

    mess around with type classes

--- Report.hs
+++ Report.hs
@@ -14,3 +14,15 @@ addToReport (Report c w ln, last) x
     | x == '\n' && last /= ' ' && last /= '\n' = (Report  c      (w + 1) (ln + 1), x)
     | x == '\n'                                = (Report  c       w      (ln + 1), x)
     | otherwise                                = (Report (c + 1)  w       ln,      x)
+
+
+reportMap f (Report a b c) = Report (f a) (f b) (f c)
+
+-- --------------------------------------------------------
+
+data Report2 a = Report2 a a a deriving (Show)
+
+-- What's the purpose of type classes again?
+instance Functor Report2 where
+    fmap f (Report2 a b c) = Report2 (f a) (f b) (f c)
+
